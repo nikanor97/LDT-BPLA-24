94348af67974ebe26c41850fa70b70052cedc3b2
nikita <nikanor97@gmail.com>
2024-06-26 03:56:49 +0300

    - added lock for redis

--- backend/src/server/amqp_processors.py
+++ backend/src/server/amqp_processors.py
@@ -1,6 +1,7 @@
 import ast
 import json
 import tempfile
+import time
 from collections import defaultdict
 from pathlib import Path
 from uuid import UUID
@@ -60,6 +61,44 @@ def add_annotations(image: Image, annotations: list[dict], label_by_id: dict[UUI
             label_name = label_by_id[annotation['label_id']].name
             text = f"{label_name}: {confidence:.2f}"
             draw.text((top_left[0], top_left[1] - 10), text, fill="red")
+
+
+class RedisLockManager:
+    def __init__(self, client, lock_name, lock_timeout):
+        self.client = client
+        self.lock_name = lock_name
+        self.lock_timeout = lock_timeout
+        self.lock_key = f"lock:{lock_name}"
+        self.lock_value = None
+
+    def acquire_lock(self):
+        self.lock_value = str(time.time() + self.lock_timeout + 1)
+
+        if self.client.set(self.lock_key, self.lock_value, nx=True, px=self.lock_timeout):
+            return True
+        else:
+            # Проверка истекшей блокировки
+            current_value = self.client.get(self.lock_key)
+            if current_value and float(current_value) < time.time():
+                old_value = self.client.getset(self.lock_key, self.lock_value)
+                if old_value == current_value:
+                    return True
+
+        return False
+
+    def release_lock(self):
+        if self.lock_value:
+            current_value = self.client.get(self.lock_key)
+            if current_value and current_value.decode('utf-8') == self.lock_value:
+                self.client.delete(self.lock_key)
+
+    def __enter__(self):
+        if not self.acquire_lock():
+            raise RuntimeError("Cannot acquire lock")
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.release_lock()
 
 
 async def yolo_markup_processor(
@@ -181,9 +220,18 @@ async def yolo_markup_processor(
 
                             # Убеждаемся, что отправка уведомления в телеграм произойдет
                             # строго один раз (а не в каждом воркере)
-                            redis_client.incr(str(content.id) + "_notification_sent")
-                            if int(redis_client.get(str(content.id) + "_notification_sent").decode()) == settings.GUNICORN_WORKERS:
-                                await notify_user(application, project.msg_receiver, temp_image_path, caption)
+
+                            lock_name = str(content.id) + "_notification_sent"
+                            lock_timeout = 10000  # 10 секунд
+
+                            try:
+                                logger.info(f"Trying to acquire lock for Redis for notification sending (lock name: {lock_name})")
+                                with RedisLockManager(redis_client, lock_name, lock_timeout) as lock:
+                                    redis_client.incr(str(content.id) + "_notification_sent")
+                                    if int(redis_client.get(str(content.id) + "_notification_sent").decode()) == 1:
+                                        await notify_user(application, project.msg_receiver, temp_image_path, caption)
+                            except RuntimeError:
+                                logger.info(f"Cannot acquire lock for Redis for notification sending (lock name: {lock_name})")
 
     async with main_db_manager.projects.make_autobegin_session() as session:
         await main_db_manager.projects.increase_content_detected_cnt(session, content.id, n_new_markups)
